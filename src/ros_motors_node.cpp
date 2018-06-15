08314f51448bbcd708ba958d0bd3d7a9fd774fcc
Kiguchi <vitor-kiguchi@hotmail.com>
2018-06-15 19:50:01 -0300

    adding contents of trekking_node/serial

--- src/ros_motors_node.cpp
+++ src/ros_motors_node.cpp
@@ -24,17 +24,123 @@
 
 #include <ros/ros.h>
 #include <iostream>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-//#include <thunder_trekking/Motor.h>
-#include "trekking_node/serial.h"
 
 #define INIT_VMAX 255
 
 #define abs(x) ((x) > 0 ? (x) : -(x))
 
+#define _DEFAULT_SOURCE
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#define OPTIONS "b:d:h"
+#define nitens(list) 1 [&list] - list
+#define try(cmd, msg) do { if ((cmd) == -1) { std::perror(msg); return -1; } } while (0)  // NOLINT(whitespace/braces)
 
+// Ver man 3 termios para as flags para terminais
+
+/*
+ * Possíveis constantes de baud:
+ * B0
+ * B50
+ * B75
+ * B110
+ * B134
+ * B150
+ * B200
+ * B300
+ * B600
+ * B1200
+ * B1800
+ * B2400
+ * B4800
+ * B9600
+ * B19200
+ * B38400
+ * B57600
+ * B115200
+ * B230400
+ * Tem valores consecutivos (http://www.delorie.com/djgpp/doc/incs/termios.h). Uma busca binária em uma array com os
+ * valores retorna o valor certo da constante.
+ */
+namespace serial{
+	static const unsigned int baud_list[] =
+	{ 0,    50,   75,   110,  134,   150,   200,   300,    600,   1200,
+	  1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400
+	};
+
+	static int cmp_uints(const void *cmp1, const void *cmp2)
+	{
+	  const unsigned int *n1 = reinterpret_cast<const unsigned int *>(cmp1);
+	  const unsigned int *n2 = reinterpret_cast<const unsigned int *>(cmp2);
+	  return *n1 - *n2;
+	}
+
+	inline static int baud_bsearch(const int *baud)
+	{
+	  unsigned int *found = NULL;
+
+	  found = (unsigned int *)bsearch(baud, &baud_list, nitens(baud_list), sizeof(baud_list[0]), cmp_uints);
+	  if (!found)
+	    return -1;
+	  return found - baud_list;
+	}
+
+	/***********************************************************************
+	 * serial_open
+	 * Abre um dispositivo serial. O driver para terminais do Linux suporta
+	 * várias flags e configurações, essa função abre:
+	 * -Com baud de entrada e saída iguais
+	 * -Sem checagem de paridade
+	 * -Sem processamento de saída ou geração de sinais
+	 * -Transmissão de 8 bits
+	 * -Modo canônico
+	 **********************************************************************/
+	int serial_open(char *dev, const int *baud, int flags)
+	{
+	  struct termios tty;
+	  int fd, speed;
+
+	  // Baud de entrada/saída
+	  if ((speed = baud_bsearch(baud)) == -1)
+	  {
+	    fprintf(stderr, "serial_open - Invalid baud rate\n");
+	    return -1;
+	  }
+
+	  try
+	    (fd = open(dev, flags | O_NOCTTY), "serial_open - open");
+
+	  memset(&tty, 0, sizeof(tty));
+	  cfsetospeed(&tty, (speed_t)speed);
+	  cfsetispeed(&tty, (speed_t)speed);
+
+	  // Sem paridade
+	  tty.c_iflag = IGNPAR;
+
+	  // Sem processamento adicional da saída
+	  // (ver manual, há flags para conversão entre convenções como NL -> CRNL automáticas)
+	  tty.c_oflag = 0;
+
+	  // 8 bits de dados, mas a especificação diz que o bit 7 é sempre 0 para NMEA
+	  tty.c_cflag |= (CS8 | CLOCAL | CREAD);
+
+	  // VMIN e VTIME setam número mínimo de caracteres para ler antes de retornar read
+	  // e timeout
+
+	  // Modo canônico
+	  tty.c_lflag = ICANON;
+	  tcflush(fd, TCIFLUSH);
+	  try
+	    (tcsetattr(fd, TCSAFLUSH, &tty), "serial_open - tcsetattr");
+	  return fd;
+	}  // namespace Trekking
+}
 
 class Motor
 {
@@ -77,7 +183,7 @@ void Motor::init_serial(){
 	}
 }
 
-void Motor::drive_serial(int servo, int speed){
+void Motor::drive_serial(){
 	init_serial();
 	uint8_t message[6] = { 255, servo < 0, abs(servo), speed < 0, abs(speed), 254 };
 	if (write(fd, message, sizeof(message)) == -1){
@@ -85,10 +191,11 @@ void Motor::drive_serial(int servo, int speed){
 		close(fd);
 		init_serial();
     }
-	cur_speed = speed;
 }
-void Motor::drive_serial(){
-	drive_serial(servo, speed);
+void Motor::drive_serial(int c_servo, int c_speed){
+	this->servo = c_servo;
+	this->speed = c_speed;
+	drive_serial();
 }
 
 void Motor::set_max_speed(int new_max_speed){
